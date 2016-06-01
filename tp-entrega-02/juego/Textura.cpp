f31776db213e5ede25404b5e381e8a97de5777e5
nickzaro <nickzaro@gmail.com>
2016-05-31 21:42:18 -0300

    Textura era copia de ejemplo sin modificacion.

--- tp-entrega-02/juego/Textura.cpp
+++ tp-entrega-02/juego/Textura.cpp
@@ -2,7 +2,6 @@
 
 Textura::Textura()
 {
-	//Initialize
 	mTextura = NULL;
 	mWidth = 0;
 	mHeight = 0;
@@ -13,50 +12,37 @@ Textura::~Textura()
 	liberar();
 }
 
-bool Textura::cargarDeArchivo( std::string path, SDL_Renderer* gRenderer)
-{
-	//Get rid of preexisting texture
+bool Textura::cargarDeArchivo( std::string path, SDL_Renderer* gRenderer){
 	liberar();
-
-	//The final texture
 	SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
 	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
 	if( loadedSurface == NULL )
 	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+		printf( "no se puede cargar la imagen %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
 	}
 	else
 	{
-		//Color key image
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
-
-		//Create texture from surface pixels
         newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
 		if( newTexture == NULL )
 		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+			printf( "No se puede creaar la textura %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
 		}
 		else
 		{
-			//Get image dimensions
+			// cargar las dimensiones
 			mWidth = loadedSurface->w;
 			mHeight = loadedSurface->h;
 		}
-
-		//Get rid of old loaded surface
 		SDL_FreeSurface( loadedSurface );
 	}
-
-	//Return success
 	mTextura = newTexture;
 	return mTextura != NULL;
 }
 
 void Textura::liberar()
 {
-	//Free texture if it exists
 	if( mTextura != NULL )
 	{
 		SDL_DestroyTexture( mTextura );
@@ -64,24 +50,6 @@ void Textura::liberar()
 		mWidth = 0;
 		mHeight = 0;
 	}
-}
-
-void Textura::setColor( Uint8 red, Uint8 green, Uint8 blue )
-{
-	//Modulate texture rgb
-	SDL_SetTextureColorMod( mTextura, red, green, blue );
-}
-
-void Textura::setBlendMode( SDL_BlendMode blending )
-{
-	//Set blending function
-	SDL_SetTextureBlendMode( mTextura, blending );
-}
-		
-void Textura::setAlpha( Uint8 alpha )
-{
-	//Modulate texture alpha
-	SDL_SetTextureAlphaMod( mTextura, alpha );
 }
 
 int Textura::getWidth()
@@ -96,16 +64,11 @@ int Textura::getHeight()
 
 void Textura::render( int x, int y,SDL_Renderer* gRenderer, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
 {
-	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
 
-	//Set clip rendering dimensions
-	if( clip != NULL )
-	{
+	if( clip != NULL ){
 		renderQuad.w = clip->w;
 		renderQuad.h = clip->h;
 	}
-
-	//Render to screen
 	SDL_RenderCopyEx( gRenderer, mTextura, clip, &renderQuad, angle, center, flip );
 }
