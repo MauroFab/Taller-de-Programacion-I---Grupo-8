1a7435ae33c37e8fe372880fe7a8e40f7971c8b2
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2016-06-18 21:05:31 -0300

    Agregadas clases para el manejo de fuentes y el ingreso de texto.

---
+++ tp-entrega-03/juego/TextField.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_ttf.h>
+
+#include "TexturaFuente.h"
+#include <string>
+
+class TextField {
+
+public:
+	TextField(SDL_Renderer* renderer, TTF_Font* font);
+	virtual ~TextField(void);
+
+	std::string getTexto();
+	void setPosicion(int x, int y);
+	void manejarEvento(SDL_Event evento);
+	void render();
+	void limpiar();
+
+private:
+	SDL_Renderer* renderer;
+	TTF_Font* font;
+	SDL_Color color;
+	std::string texto;
+	bool renderTextFlag;
+	TexturaFuente* textura;
+	int posX;
+	int posY;
+};
+
