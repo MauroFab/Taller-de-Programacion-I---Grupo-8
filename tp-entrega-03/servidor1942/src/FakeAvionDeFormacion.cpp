f8bc2bff2ab79841230c62ff56d8a986ea0c10c6
MauroFab <maurotoscano2@gmail.com>
2016-06-27 23:24:25 -0300

    Acomodando la logica del giro

--- tp-entrega-03/servidor1942/src/FakeAvionDeFormacion.cpp
+++ tp-entrega-03/servidor1942/src/FakeAvionDeFormacion.cpp
@@ -1,4 +1,3 @@
-
 #include "FakeAvionDeFormacion.h"
 
  FakeAvionDeFormacion::FakeAvionDeFormacion(int y, int alturaDeLaQueSalgo) : 
@@ -9,10 +8,12 @@
 	puntosQueOtorgaAlSerDestruido = 100;
 	this->alturaDeLaQueSalgo = alturaDeLaQueSalgo;
 	listoParaSalir = false;
+	this->estoyHaciendoElGiro = false;
 	this->angulo = 0.0;
 }
 
 void FakeAvionDeFormacion::continuarMovimiento(std::list<SuperficieOcupada> superficiesAvionesJugadores){
+	
 	if(superficieOcupada->obtenerPosicion().getPosY() < alturaDeLaQueSalgo){
 		listoParaSalir = true;
 	}
@@ -20,20 +21,35 @@ void FakeAvionDeFormacion::continuarMovimiento(std::list<SuperficieOcupada> supe
 		//Me voy moviendo con el mapa
 		superficieOcupada->desplazarEnYObteniendoHitbox(-1);
 	}else{
-		if (this->angulo < 100){
+		if(!estoyHaciendoElGiro){
+			superficieOcupada->desplazarEnXObteniendoHitbox(-velocidadAvionMini);
+		}
+		if(superficieOcupada->obtenerPosicion().getPosX() == 200){
+			estoyHaciendoElGiro = true;
+		}
+
+		if(estoyHaciendoElGiro){
+			//continuoElGiro
 			this->angulo += DELTA_TITA;
 			UtilJuego * utilJ = UtilJuego::getInstance();
 			double pX = this->superficieOcupada->x;
 			double pY = this->superficieOcupada->y;
 			utilJ->updatePolarToCartesiana(V_RADIO,this->angulo,&pX,&pY);
+			this->superficieOcupada->moverAPosicion(Posicion(static_cast<int> (pX),static_cast<int> (pY)));
+			if(angulo > 360)
+				estoyHaciendoElGiro = false;
+		}
+		/*if (this->angulo < 100){
+			
+			
+			
+			
 			this->superficieOcupada->x = pX;
 			this->superficieOcupada->y= pY;
 		}
 		else{
-			superficieOcupada->desplazarEnXObteniendoHitbox(-velocidadAvionMini);
-		//En algun x habria que empezar un giro. Junto con este hay que actualizar la frame.
-		//incrementar el angulo
-		}
+			
+		}*/
 
 	}
 	
