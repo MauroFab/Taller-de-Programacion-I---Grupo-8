6f9f9455225e5a2c9ecf280ea533809717cbcd27
nickzaro <nickzaro@gmail.com>
2016-07-12 02:48:03 -0300

    operador asignación por ser usado, evitar problemas.

--- tp-entrega-04/juego/EstadoJugador.h
+++ tp-entrega-04/juego/EstadoJugador.h
@@ -10,7 +10,12 @@ class EstadoJugador {
 
 public:
 	EstadoJugador(int id, int puntajeAcumulado, int equipo, string nombre);
-
+	
+	EstadoJugador(const EstadoJugador& e):id(e.id),puntajeAcumulado(e.puntajeAcumulado),
+	equipo(e.equipo),nombre(e.nombre){};
+	void EstadoJugador::operator=(const EstadoJugador &e){this->id=e.id; this->puntajeAcumulado=e.puntajeAcumulado;
+	this->equipo=e.equipo;this->nombre=e.nombre;};
+	
 	//No usar este constructor por fuera del protocolo
 	EstadoJugador();
 
